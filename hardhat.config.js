32d6b8b97936b33811e064999c335230143fa113
Camila Lo <camilalo.tw@gmail.com>
2022-06-10 15:38:26 +0800

    update deploy script and hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,21 @@
+require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require("dotenv").config();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ENDPOINT_URL = process.env.ENDPOINT_URL;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ENDPOINT_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
 };
